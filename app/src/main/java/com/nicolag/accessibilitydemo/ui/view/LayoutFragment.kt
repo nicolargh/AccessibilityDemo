33e3a191a70130c2f20132aa75fa18d0eebbba14
Nicola Gibson <nigibso@yammer-inc.com>
2019-07-14 15:06:33 -0700

    Add second layout to layout demo

--- app/src/main/java/com/nicolag/accessibilitydemo/ui/view/LayoutFragment.kt
+++ app/src/main/java/com/nicolag/accessibilitydemo/ui/view/LayoutFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import com.nicolag.accessibilitydemo.R
 import com.nicolag.accessibilitydemo.injection.AppComponent
 import kotlinx.android.synthetic.main.card_1.*
+import kotlinx.android.synthetic.main.card_2.*
 
 class LayoutFragment : BaseFragment() {
     override fun createView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -16,15 +17,25 @@ class LayoutFragment : BaseFragment() {
 
     override fun setupView() {
         val name = "Other User"
-        avatar.contentDescription = requireContext().getString(R.string.avatar_content_description)
-        avatar.setOnClickListener { showToast("Opening $name's profile") }
-        title.text = name
-        title.setOnClickListener { showToast("Opening $name's profile") }
-        content.setOnClickListener { showToast("Opening full conversation") }
-        likes.setOnClickListener { showToast("Opening 'who liked this' list") }
-        heart.setOnClickListener { showToast("Liking...") }
-        comment.setOnClickListener { showToast("Commenting...") }
-        share.setOnClickListener { showToast("Sharing...") }
+
+        avatar1.contentDescription = requireContext().getString(R.string.avatar_content_description, name)
+        avatar1.setOnClickListener { showToast("Opening $name's profile") }
+        title1.text = name
+        title1.setOnClickListener { showToast("Opening $name's profile") }
+        content1.setOnClickListener { showToast("Opening full conversation") }
+        likes1.setOnClickListener { showToast("Opening 'who liked this' list") }
+        heart1.setOnClickListener { showToast("Liking...") }
+        comment1.setOnClickListener { showToast("Commenting...") }
+        share1.setOnClickListener { showToast("Sharing...") }
+
+        card2.setOnClickListener { showToast("Opening full conversation") }
+        avatar2.setOnClickListener { showToast("Opening $name's profile") }
+        title2.text = name
+        title2.setOnClickListener { showToast("Opening $name's profile") }
+        likes2.setOnClickListener { showToast("Opening 'who liked this' list") }
+        heart2.setOnClickListener { showToast("Liking...") }
+        comment2.setOnClickListener { showToast("Commenting...") }
+        share2.setOnClickListener { showToast("Sharing...") }
     }
 
     private fun showToast(string: String) {
